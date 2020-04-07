00b80275f7997352f91f355d686efcf6b1704e07
Lucas Neckel <lucas.neckel@gmail.com>
2020-04-07 17:46:14 -0300

    importando css e image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import Header from './components/Header';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
 
 function App() {
   const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
@@ -16,6 +18,9 @@ function App() {
   return (
     <>
       <Header title="Projects"/> 
+
+      <img width={300} src={backgroundImage}/>
+
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
       </ul>
